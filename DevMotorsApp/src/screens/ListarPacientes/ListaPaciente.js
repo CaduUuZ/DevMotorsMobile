e713f19829dfeab63ef5bb979eb7587ec0319640
Flavio Machado <flavio.gabrielbarto02@gmail.com>
2025-10-18 22:39:20 -0300

    Import ListaPaciente

---
+++ DevMotorsApp/src/screens/ListarPacientes/ListaPaciente.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  ScrollView,
+  Alert,
+  StyleSheet,
+  SafeAreaView,
+  ActivityIndicator,
+  StatusBar
+} from 'react-native';
