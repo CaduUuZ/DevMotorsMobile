e5025e5776f7d67ffeb2e4230e85fd20439fc643
Carlos Eduardo (Cadu) <cadupaiva629@gmail.com>
2025-10-28 20:49:52 -0300

    Lista Exames e novo exame

--- DevMotorsApp/src/config/api.js
+++ DevMotorsApp/src/config/api.js
@@ -1,10 +1,11 @@
 import { Platform } from 'react-native';
 
-const HOST_IP = '10.136.130.200'; // seu IP na rede
+const HOST_IP = '201.86.43.42'; // seu IP na rede
 const PORT = 3000; // porta onde seu backend HTTP responde
 
 export const API_BASE_URL = Platform.OS === 'android'
-  ? `http://10.0.2.2:${PORT}`
+  ? `http://201.86.43.42:${PORT}`
   : `http://${HOST_IP}:${PORT}`;
 
 export const PACIENTES_ENDPOINT = `${API_BASE_URL}/pacientes`;
+export const EXAMES_ENDPOINT = `${API_BASE_URL}/exames`;
